a65f00d03cb5569e83f2eb7dad1e8c62100b5ce9
Hengky Wijaya <hannw171@outlook.com>
2020-07-27 02:32:22 +0700

    Build APP
    Add table to Admin HomePage

--- app/src/Component/HomeAdmin.jsx
+++ app/src/Component/HomeAdmin.jsx
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Jumbotron, Button, Table } from 'reactstrap';
+import React, { useContext, useEffect, useState, Fragment } from 'react'
+import { Jumbotron, Button, Table, Navbar } from 'reactstrap';
 import { AdminAuthContext } from '../App';
-import "./CSS/HomePage.css"
+import "./CSS/HomeAdmin.css"
 import axios from 'axios'
 
 const api = 'http://localhost:3002'
@@ -9,10 +9,10 @@ const api = 'http://localhost:3002'
 export default function HomeAdmin() {
     const { state, dispatch } = useContext(AdminAuthContext)
     const [anggota, setAnggota] = useState([])
-    
+
     useEffect(() => {
         console.log("Memanggil Use Effect")
-        axios.get(api + '/anggota' ).then(res => {
+        axios.get(api + '/anggota').then(res => {
             setAnggota(res.data.values)
         })
     }, [])
@@ -25,30 +25,67 @@ export default function HomeAdmin() {
     }
     return (
         <div>
-            <h2>Daftar Anggota</h2>
-            <hr />
-            <Table className="table-bordered">
-                <thead>
-                    <tr>
-                        <th>No. Anggota</th>
-                        <th>Nama</th>
-                        <th>Tempat, tanggal lahir</th>
-                        <th>Alamat</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {anggota.map(anggota =>
-                        <tr key={anggota.id_anggota}>
-                            <td>{anggota.id_anggota}</td>
-                            <td>{anggota.nama_lengkap}</td>
-                            <td>{anggota.ttl}</td>
-                            <td>{anggota.alamat}</td>
-                            <td>{anggota.status_keanggotaan}</td>
+            <Navbar className="sidebar" aria-orientation="vertical">
+            </Navbar>
+
+            <div className="tabel">
+                <p className="headerTabel">Daftar Anggota</p>
+                <Table className="tabel-head">
+                    <thead>
+                        <tr>
+                            <th>No. Anggota</th>
+                            <th>Nama</th>
+                            <th>Tempat, tanggal lahir</th>
+                            <th>Alamat</th>
+                            <th>Status</th>
+                            <th>Action</th>
                         </tr>
-                    )}
-                </tbody>
-            </Table>
+                    </thead>
+                    <tbody>
+                        {anggota.map(anggota =>
+                            <tr key={anggota.id_anggota}>
+                                <td>{anggota.id_anggota}</td>
+                                <td>{anggota.nama_lengkap}</td>
+                                <td>{anggota.ttl}</td>
+                                <td>{anggota.alamat}</td>
+                                <td>{anggota.status_keanggotaan}</td>
+                                <td>
+                                    <button>edit</button>
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+            </div>
+            <div className="tabel2">
+                <p className="headerTabel">Berita Duka Cita</p>
+                <Table className="tabel-head">
+                    <thead>
+                        <tr>
+                            <th>No. Anggota</th>
+                            <th>Nama</th>
+                            <th>Tempat, tanggal lahir</th>
+                            <th>Alamat</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {anggota.map(anggota =>
+                            <tr key={anggota.id_anggota}>
+                                <td>{anggota.id_anggota}</td>
+                                <td>{anggota.nama_lengkap}</td>
+                                <td>{anggota.ttl}</td>
+                                <td>{anggota.alamat}</td>
+                                <td>{anggota.status_keanggotaan}</td>
+                                <td>
+                                    <button>edit</button>
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+            </div>
         </div>
     )
 }
